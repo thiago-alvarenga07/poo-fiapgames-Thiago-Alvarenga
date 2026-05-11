4cb98783a28b4140be5e47dfe2da310aafdae96f
Thiago Sobral <thiago.s.alvarenga@gmail.com>
2026-05-11 10:19:37 -0300

    feat: aula4 - cria construtores para inicializar objetos

--- src/br/com/fiapgames/main/SistemaPrincipal.java
+++ src/br/com/fiapgames/main/SistemaPrincipal.java
@@ -1,28 +1,13 @@
 package br.com.fiapgames.main;
-// Importando a classe Usuario
-import br.com.fiapgames.model.Usuario;
+// Importando a classe Produto
+import br.com.fiapgames.model.Produto;
 
 public class SistemaPrincipal {
     
     public static void main(String[] args) {
+        Produto produto1 = new Produto("Pragmata", 150.0, 1);
         // Criando um novo usuário
-        Usuario usuario1 = new Usuario("Thiago Alvarenga", "123", "thiago@gmail.com");
-        System.out.println("Recarga de saldo para o usuário " + usuario1.getNome());
-        usuario1.adicionarSaldo(100.0); // Adicionando saldo ao usuário
-
-        Usuario usuario2 = new Usuario("Maria Silva", "456", "maria@gmail.com");
-        System.out.println("Recarga de saldo para o usuário " + usuario2.getNome());
-        usuario2.adicionarSaldo(150.0);
-
-        // Exibindo as informações dos usuários
-        System.out.println("--- Loja de Games ---");
-        System.out.println("Usuário: " + usuario1.getNome() + " | CPF: " + usuario1.getCpf() + " | Email: " + usuario1.getEmail() + " | Saldo: R$ " + usuario1.getSaldo());
-        System.out.println("Usuário: " + usuario2.getNome() + " | CPF: " + usuario2.getCpf() + " | Email: " + usuario2.getEmail() + " | Saldo: R$ " + usuario2.getSaldo());
-        
-        System.out.println("\n--- Realizando compras ---");
-        System.out.println("Realizando compra de jogo para " + usuario1.getNome());
-        usuario1.comprarJogo(75.0);
-        System.out.println("Realizando compra de jogo para " + usuario2.getNome());
-        usuario2.comprarJogo(200.0);
+        System.out.println("--- Produtos ---");
+        System.out.println("Produto: " + produto1.getNome() + " - Preço: R$ " + produto1.getPreco());
     }
 }
---
+++ src/br/com/fiapgames/model/Produto.java
@@ -0,0 +1,45 @@
+package br.com.fiapgames.model;
+
+public class Produto {
+    
+    // Atributos do produto
+    private String nome;
+    private double preco;
+    private int id;
+
+    public Produto(String nome, double preco, int id) {
+        this.nome = nome;
+        this.setPreco(preco);
+        this.id = id;
+        System.out.println("Produto: " + this.nome + " adicionado com o id: " + this.id);
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public double getPreco() {
+        return this.preco;
+    }
+
+    private void setPreco(double preco) {
+        if (preco > 0) {
+            this.preco = preco;
+        } else {
+            System.out.println("Erro de segurança: O preço deve ser um valor positivo.");
+        }
+    }
+
+    public void atualizarPreco(double novoPreco) {
+        if (novoPreco > 0) {
+            this.preco = novoPreco;
+            System.out.println("Preço atualizado para: R$ " + this.preco);
+        } else {
+            System.out.println("Erro de segurança: O novo preço deve ser um valor positivo.");
+        }
+    }
+
+    public int getId() {
+        return this.id;
+    }
+}
